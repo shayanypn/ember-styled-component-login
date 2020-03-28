8fe7702d2d60874218d72f65fd705cee34581ff6
shayanypn <shayanypn@gmail.com>
2020-03-28 13:21:02 +0430

    add styled component plugin

---
+++ app/components/styled/wrapper.js
@@ -0,0 +1,10 @@
+import styled from 'ember-styled-components';
+
+/**
+ * Create a wrapper component that renders a <section> with
+ * some padding and a papayawhip background
+ */
+export default styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
