cbbf420a47e962c90548e7a0f23fec678456597d
shayanypn <shayanypn@gmail.com>
2020-03-28 15:41:52 +0430

    test provide pages container

--- tests/acceptance/styled-login-test.js
+++ tests/acceptance/styled-login-test.js
@@ -9,6 +9,11 @@ module('Acceptance | styled login', function(hooks) {
     await visit('/');
     assert.equal(currentURL(), '/signin');
     assert.dom('h2').hasText('Sign In');
+
+    assert.dom('h1').hasText('AceInvoice');
+    assert.dom('h3').hasText('time tracking and invoicing');
+
+    assert.dom('a.a--signup > strong').hasText('Sign up');
   });
 
   test('visiting /signin', async function(assert) {
@@ -21,12 +26,30 @@ module('Acceptance | styled login', function(hooks) {
     await visit('/signup');
     assert.equal(currentURL(), '/signup');
     assert.dom('h2').hasText('Sign Up');
+
+    assert.dom('h1').hasText('AceInvoice');
+    assert.dom('h3').hasText('time tracking and invoicing');
   });
 
   test('visiting /profile', async function(assert) {
     await visit('/profile');
     assert.equal(currentURL(), '/profile');
     assert.dom('h2').hasText('Panel');
+  });
+
+
+  test('switch to signup from signin', async function(assert) {
+    await visit('/signin');
+
+    await click('a.a--signup');
+    assert.equal(currentURL(), '/signup');
+  });
+
+  test('switch to signin from signup', async function(assert) {
+    await visit('/signup');
+
+    await click('a.a--back');
+    assert.equal(currentURL(), '/signin');
   });
 
 });
